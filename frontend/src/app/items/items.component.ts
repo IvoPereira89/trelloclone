8e8f80bc6ccc690a514681d59885d94095951365
Pedro Samuel <p.samuel@runtime-revolution.com>
2018-06-05 18:37:13 +0100

    Preparing Item Form Component in Items Component.

--- frontend/src/app/items/items.component.ts
+++ frontend/src/app/items/items.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Inject } from "@angular/core";
+import { Component, OnInit, OnDestroy, Input, Inject } from "@angular/core";
 
 @Component({
   selector: "app-items",
   template: `
-     <div (click)="selectItem($event, item.id)">
-       <h4>{{item.title}}</h4>
+     <div>
+       <h4 class="deb" (click)="selectItem($event, item)">{{item.title}}</h4>
        <small>{{item.description}}</small>
      </div>
   `,
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-
   @Input() item;
 
   constructor(@Inject("items-service") private itemsService) {}
@@ -20,7 +18,31 @@ export class ItemsComponent implements OnInit {
   ngOnInit() {
   }
 
-  selectItem(event, id) {
-    console.log(id)
+  selectItem(event, item) {
+    const element = event.target;
+    element.parentElement.innerHTML = form;
+  }
+}
+
+@Component({
+  selector: 'app-item-form',
+  template: `
+    <div id="item-form">
+    <div>
+      <label for="title">Title</label>
+      <input name="title" type="text" value={{item.title}}></input>
+    </div>
+    <div>
+      <label for="description">Description</label>
+      <input name="description" type="text" value={{item.description}}></input>
+    </div>
+    <button type="button" (click)="saveItem($event)">Save</button>
+  </div>`
+})
+export class ItemForm implements OnInit, OnDestroy {
+  @Input() item;
+
+  saveItem(event){
+
   }
 }
